605523ae6200c2ff29ea11d339251b89064ce929
Daxx <daxxpharellflores@gmail.com>
2026-08-29 19:02:19 -0700

    fix typo

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/oqwdweprxbbrcnyrsbpq'
+const SUPABASE_URL = 'https://oqwdweprxbbrcnyrsbpq.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_wcGMJZie73uffyOZpIcfoQ_ZrsAceR-'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
